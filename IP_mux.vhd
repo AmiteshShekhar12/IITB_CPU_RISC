c6d5379dc64c95e9deeac7c4752a9f4d1dbb82aa
AmiteshShekhar12 <132191482+AmiteshShekhar12@users.noreply.github.com>
2023-12-02 14:28:46 +0530

    Update IP_mux.vhd

--- IP_mux.vhd
+++ IP_mux.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity IP_mux is
-	port (ALU_C, T2 : in std_logic_vector (15 downto 0);
+	port (ALU_C, T2 , IP_in: in std_logic_vector (15 downto 0);
 			state : in std_logic_vector (4 downto 0);
 			ALU_Z : in std_logic;
 			IP_mux_out : out std_logic_vector (15 downto 0));
@@ -20,7 +20,7 @@ signal s1, s14, s16, s18 : std_logic := '0';
 	s18 <= state(4) and not(state(3)) and not(state(2)) and state(1) and not(state(0));
 	
 	g1 : for i in 0 to 15 generate
-		IP_mux_out(i) <= ((s1 or (s14 and ALU_Z) or s16) and ALU_C(i)) or (s18 and T2(i));
+		IP_mux_out(i) <= ((s1 or (s14 and ALU_Z) or s16) and ALU_C(i)) or (s18 and T2(i)) or (s14 and not(ALU_Z)) and IP_in;
 	end generate g1;
 	
 end struct;
